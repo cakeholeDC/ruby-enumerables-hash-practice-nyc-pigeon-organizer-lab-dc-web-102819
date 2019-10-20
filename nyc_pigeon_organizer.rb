7ca9e2ca49de5de03770007fde6a1204037333ab
Kyle Cole <kylepcole@gmail.com>
2019-10-20 22:01:35 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,15 +2,15 @@ def nyc_pigeon_organizer(data)
   pigeon_hash = {}
   
   data[:gender].each do |sex|
-    puts "sex = #{sex.values[0]}"
-    sex.values.each do |name|
-     puts "name = #{name}"
-     pigeon_hash[data[:gender][name]] = {
-        :color => [],
-        :gender => [],
-        :lives => []
-      }
-    end # |sex|.values.each
+    puts "sex = #{sex}"
+    #sex.values.each do |name|
+     #puts "name = #{name}"
+     #pigeon_hash[data[:gender][name]] = {
+      #  :color => [],
+      #  :gender => [],
+      #  :lives => []
+      #}
+    #end # |sex|.values.each
   end
     
   puts pigeon_hash
