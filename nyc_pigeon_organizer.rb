a6e2ac9d4b52c30bd1a3b40b4ee7deab5e76af5e
Kyle Cole <kylepcole@gmail.com>
2019-10-20 21:54:24 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,12 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_hash = {}
+  
+  data[:gender].each do |name|
+    pigeon_hash[name] = {
+      :color => '',
+      :gender => '',
+      :lives => ''
+    }
+    
+  puts pigeon_hash
 end
