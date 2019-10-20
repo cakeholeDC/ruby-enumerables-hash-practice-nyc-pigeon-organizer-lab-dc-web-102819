6ea5eb4b9ba503246961635f02b72943d67914d0
Kyle Cole <kylepcole@gmail.com>
2019-10-20 22:22:25 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,14 +2,17 @@ def nyc_pigeon_organizer(data)
   pigeon_hash = {}
   
   data.each do |attribute, attr_values|
+    puts "data.each...."
     puts attribute
     puts attr_values
     
     attr_values.each do |value, names|
+      puts "attr_values.each...."
       puts value
       puts names
       
       names.each do |name|
+        puts "names.each...."
         puts name
       end
     end
